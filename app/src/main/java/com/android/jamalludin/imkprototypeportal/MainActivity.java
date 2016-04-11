b3ec93b4d435ec9b7270d1614749ea8326e10d25
Jamalludin <jamalludin1209@gmail.com>
2016-04-11 18:41:55 +0700

    Menambahkan login dan Dasboard

--- app/src/main/java/com/android/jamalludin/imkprototypeportal/MainActivity.java
+++ app/src/main/java/com/android/jamalludin/imkprototypeportal/MainActivity.java
@@ -1,13 +1,50 @@
 package com.android.jamalludin.imkprototypeportal;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AlertDialog.Builder alertDialogBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (alertDialogBuilder == null) {
+                alertDialogBuilder = new AlertDialog.Builder(this);
+            }
+
+            alertDialogBuilder.setTitle("Yakin untuk Keluar ?");
+            alertDialogBuilder
+                    .setMessage("Klik YA untuk keluar..")
+                    .setCancelable(false)
+                    .setPositiveButton("YA",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+                                    // menghancurkan activity / keluar aplikasi
+                                    finish();
+                                }
+                            })
+                    .setNegativeButton("TIDAK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+
+                                    dialog.cancel();
+                                }
+                            }).create().show();
+
+        }
+        return false;
     }
 }
